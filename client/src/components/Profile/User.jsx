4db2f49f8fca41d247760f9a4da111c3c1ab6604
MarkivV <markivvv07@gmail.com>
2022-04-10 17:17:54 +0200

    big update

--- client/src/components/Profile/User.jsx
+++ client/src/components/Profile/User.jsx
@@ -92,7 +92,7 @@ const User = () => {
                                         {/*<Card hoverable className={"news-card"} style={{height: "550px"}}>*/}
                                         <a href={item.url} target={"_blank"} rel={"noreferrer"}>
                                             <div className="news-image-container">
-                                                <img style={{width: '100%'}} src={news.image} alt={"news"}/>
+                                                <img style={{width: '100%'}} src={item.image} alt={"news"}/>
                                             </div>
                                             <div style={{marginLeft: "0px"}}>
                                                 <Title level={4}>{item.name}</Title>
