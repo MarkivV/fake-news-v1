ecb71d49c09bce200e85463e18ad82a75ba7717f
MarkivV <markivvv07@gmail.com>
2022-04-29 20:53:44 +0200

    try

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import './index.css';
 import App from './App';
 
 import {BrowserRouter as Router} from "react-router-dom";
-// import "antd/dist/antd.css"
+import "antd/dist/antd.css"
 
 
 ReactDOM.render(
